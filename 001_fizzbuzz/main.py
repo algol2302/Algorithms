e023862b461151addae5ab48bef5ff6fa8254764
Alexander Golovnya <algol.mail@gmail.com>
2021-02-06 14:56:53 +0700

    Add fizzbuzz.

---
+++ 001_fizzbuzz/main.py
@@ -0,0 +1,21 @@
+def fizzbuzz():
+    # loop
+    for i in range(1, 101):
+        fizz = 'fizz' * (not i % 3)
+        buzz = 'buzz' * (not i % 5)
+        print(f'{fizz}{buzz}' or i)
+
+    # lambda
+    fizz = 'fizz'
+    buzz = 'buzz'
+    print(
+        *map(
+            lambda x: f'{fizz * (not x % 3)}{buzz * (not x % 5)}' or f'{x}',
+            range(1, 101)
+        ),
+        sep='\n'
+    )
+
+
+if __name__ == '__main__':
+    fizzbuzz()
