fd4993e96a91dcb8950df8ed4f7549c2c074c324
Alexander Golovnya <algol.mail@gmail.com>
2021-02-14 14:50:56 +0700

    Add max function with using recursion.

--- 004_recursions/main.py
+++ 004_recursions/main.py
@@ -12,19 +12,21 @@ def recursive_count(arr: list) -> int:
         return 0
     return 1 + recursive_count(arr=arr[1:])
 
-# TODO
-# def recursive_max(arr: list) -> int:
-#     if len(arr) == 0:
-#         raise ValueError('max() arg is an empty sequence')
-#
-#     if len(arr) == 1:
-#         return arr[0]
-#     else:
-#         return recursive_max(arr=arr[1:])
+
+def recursive_max(arr: list) -> int:
+    if len(arr) == 0:
+        raise ValueError('recursive_max() arg is an empty sequence')
+
+    if len(arr) == 1:
+        return arr[0]
+
+    rest = recursive_max(arr=arr[1:])
+
+    return rest if rest > arr[0] else arr[0]
 
 
 if __name__ == '__main__':
-    init_arr = [random.randint(0, 10) for _ in range(10)]
+    init_arr = [random.randint(0, 1000) for _ in range(10)]
 
     total_sum = recursive_sum(init_arr)
     check_sum = sum(init_arr)
@@ -32,8 +34,12 @@ if __name__ == '__main__':
     total_count = recursive_count(init_arr)
     check_count = len(init_arr)
 
+    max_value = recursive_max(init_arr)
+    max_check = max(init_arr)
+
     print(
         f"Initial values: {init_arr}\n"
         f"Sum: {total_sum}\t\tcheck: {total_sum == check_sum}\n"
-        f"Count: {total_count}\tcheck: {total_count == check_count}\n"
+        f"Count: {total_count}\t\tcheck: {total_count == check_count}\n"
+        f"Max: {max_value}\t\tcheck: {max_value == max_check}\n"
     )
