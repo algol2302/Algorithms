e9e1a2f4374d111e51e32250de28b6907c43536a
Alexander Golovnya <algol.mail@gmail.com>
2021-03-05 10:43:32 +0700

    Add first optimization for two sum task.

--- 009_two_sum/main.py
+++ 009_two_sum/main.py
@@ -15,10 +15,31 @@ def two_sum(nums: List[int], target: int) -> List[int]:
     return []
 
 
+def two_sum_optimized_1(nums: List[int], target: int) -> List[int]:
+    """First optimization"""
+
+    _nums = list(enumerate(nums))
+    # or:
+    # _nums = list(zip(range(len(nums)), nums))
+
+    for first_index, first in _nums:
+        for second_index, second in _nums[first_index+1:]:
+            if target - first == second:
+                return [first_index, second_index]
+
+    return []
+
+
 def main():
     print(two_sum([2, 7, 11, 15], 9))
     print(two_sum([3, 2, 4], 6))
     print(two_sum([3, 3], 6))
+
+    print('--------')
+
+    print(two_sum_optimized_1([2, 7, 11, 15], 9))
+    print(two_sum_optimized_1([3, 2, 4], 6))
+    print(two_sum_optimized_1([3, 3], 6))
 
 
 if __name__ == '__main__':
