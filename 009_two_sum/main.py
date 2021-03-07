f5f19c7c53538ba1cc3fa7d3ad9052988f5a2778
Alexander Golovnya <algol.mail@gmail.com>
2021-03-07 20:03:18 +0700

    Add second optimization for two sum task.

--- 009_two_sum/main.py
+++ 009_two_sum/main.py
@@ -18,28 +18,44 @@ def two_sum(nums: List[int], target: int) -> List[int]:
 def two_sum_optimized_1(nums: List[int], target: int) -> List[int]:
     """First optimization"""
 
-    _nums = list(enumerate(nums))
-    # or:
-    # _nums = list(zip(range(len(nums)), nums))
-
-    for first_index, first in _nums:
-        for second_index, second in _nums[first_index+1:]:
+    for first_index, first in enumerate(nums):
+        for second_index, second in enumerate(nums[first_index+1:], start=first_index+1):
             if target - first == second:
                 return [first_index, second_index]
 
     return []
 
 
+def two_sum_optimized_2(nums: List[int], target: int) -> List[int]:
+    """Second optimization"""
+
+    _helper = {}
+    for first_index, key in enumerate(nums):
+        second = target - key
+
+        try:
+            if first_index != _helper[key]:
+                return [first_index, _helper[key]]
+        except KeyError:
+            pass
+
+        _helper[second] = first_index
+
+    return []
+
+
 def main():
-    print(two_sum([2, 7, 11, 15], 9))
-    print(two_sum([3, 2, 4], 6))
-    print(two_sum([3, 3], 6))
-
-    print('--------')
-
     print(two_sum_optimized_1([2, 7, 11, 15], 9))
     print(two_sum_optimized_1([3, 2, 4], 6))
     print(two_sum_optimized_1([3, 3], 6))
+    print(two_sum_optimized_1([1, 3, 4, 2], 6))
+
+    print('--------')
+
+    print(two_sum_optimized_2([2, 7, 11, 15], 9))
+    print(two_sum_optimized_2([3, 2, 4], 6))
+    print(two_sum_optimized_2([3, 3], 6))
+    print(two_sum_optimized_2([1, 3, 4, 2], 6))
 
 
 if __name__ == '__main__':
