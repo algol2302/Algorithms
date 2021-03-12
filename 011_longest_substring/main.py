f3148491c7858e72da94b47fc18f0f3cb648e5cb
Alexander Golovnya <algol.mail@gmail.com>
2021-03-12 11:21:53 +0700

    Add solution for "Length Of Longest Substring" task.

--- 011_longest_substring/main.py
+++ 011_longest_substring/main.py
@@ -1,40 +1,81 @@
-def lengthOfLongestSubstring(s: str) -> int:
-    longest = set()
+def lengthOfLongestSubstring1(s: str) -> int:
+    """Sliding window solution with index detection inside of loop"""
+    max_length = 0
     current = []
 
     for char in s:
-        # TODO
+
+        if char in current:
+            first_index = current.index(char)
+            current = current[first_index+1:]
+
         current.append(char)
-        if len(longest) <= len(current) and "".join(current) in s:
-            longest.update(current)
-        else:
-            current = []
 
-    print(longest)
+        max_length = max(max_length, len(current))
 
-    return len(longest)
+    return max_length
+
+
+def lengthOfLongestSubstring2(s: str) -> int:
+    """Sliding window solution with index detection via hashmap"""
+
+    # TODO fix it
+    max_length = 0
+    shift = 0
+    current = []
+    items = {}
+
+    for index, char in enumerate(s):
+        if char in current:
+            shift = items[char]
+            current = current[shift+1:]
+
+        items[char] = index - shift
+        current.append(char)
+
+        max_length = max(max_length, len(current))
+
+    return max_length
 
 
 def main():
+    lengthOfLongestSubstring = lengthOfLongestSubstring2
+
     # Input: s = "abcabcbb"
     # Output: 3
-    s = "abcabcbb"
-    print(f"{s=}\tgot: {lengthOfLongestSubstring(s)} expected: 3")
+    # s = "abcabcbb"
+    # print(f"{s=}\tgot: {lengthOfLongestSubstring(s)} expected: 3")
+    #
+    # # Input: s = "bbbbb"
+    # # Output: 1
+    # s = "bbbbb"
+    # print(f"{s=}\t\tgot: {lengthOfLongestSubstring(s)} expected: 1")
+    #
+    # # Input: s = "pwwkew"
+    # # Output: 3
+    # s = "pwwkew"
+    # print(f"{s=}\t\tgot: {lengthOfLongestSubstring(s)} expected: 3")
+    #
+    # # Input: s = ""
+    # # Output: 0
+    # s = ""
+    # print(f"{s=}\t\t\tgot: {lengthOfLongestSubstring(s)} expected: 0")
+    #
+    # # Input: s = "dvdf"
+    # s = "dvdf"
+    # # Expected: 3
+    # print(f"{s=}\t\t\tgot: {lengthOfLongestSubstring(s)} expected: 3")
+    #
+    # s = "tmmzuxt"
+    # # Expected: 3
+    # print(f"{s=}\t\tgot: {lengthOfLongestSubstring(s)} expected: 5")
+    #
+    # s = "aabaab!bb"
+    # # Expected: 3
+    # print(f"{s=}\t\tgot: {lengthOfLongestSubstring(s)} expected: 3")
 
-    # Input: s = "bbbbb"
-    # Output: 1
-    s = "bbbbb"
-    print(f"{s=}\t\tgot: {lengthOfLongestSubstring(s)} expected: 1")
-
-    # Input: s = "pwwkew"
-    # Output: 3
-    s = "pwwkew"
-    print(f"{s=}\t\tgot: {lengthOfLongestSubstring(s)} expected: 3")
-
-    # Input: s = ""
-    # Output: 0
-    s = ""
-    print(f"{s=}\t\t\tgot: {lengthOfLongestSubstring(s)} expected: 0")
+    s = "bbtablud"
+    print(f"{s=}\t\tgot: {lengthOfLongestSubstring(s)} expected: 6")
 
 
 if __name__ == '__main__':
