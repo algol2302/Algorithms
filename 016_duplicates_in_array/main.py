63b2fff9740a6ac7a6f6f0ebdada405011f6be6a
Alexander Golovnya <algol.mail@gmail.com>
2021-04-01 10:54:11 +0700

    Add find duplicates solution.

---
+++ 016_duplicates_in_array/main.py
@@ -0,0 +1,35 @@
+def find_duplicates_0(values: list) -> list:
+    """Should be O(N) time complexity"""
+    duplicates = []
+    # + O(N) for space complexity
+    check = {}
+
+    for item in values:
+        if not check.get(item):
+            check[item] = item
+        else:
+            duplicates.append(item)
+    return duplicates
+
+
+def find_duplicates(values: list) -> list:
+    """Should be O(N) time complexity.
+    Values are positive and less than len(values)
+    """
+    duplicates = []
+
+    for item in values:
+        if values[abs(item)] >= 0:
+            values[abs(item)] = -values[abs(item)]
+        else:
+            duplicates.append(abs(item))
+    return duplicates
+
+
+def main():
+    print(find_duplicates([1, 2, 3, 1, 4]))
+    print(find_duplicates([2, 3, 1, 2, 3]))
+
+
+if __name__ == '__main__':
+    main()
