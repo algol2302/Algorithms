c2e4c669dce4be6d9c5995aa79a71d01a6da6a9f
Alexander Golovnya <algol.mail@gmail.com>
2021-06-09 00:06:47 +0700

    Add depth-first search implementation with recursion.

--- 024_depth_first_search/realization.py
+++ 024_depth_first_search/realization.py
@@ -28,6 +28,16 @@ def depth_first_search(start_node: Node) -> None:
                 stack.append(node)
 
 
+def depth_first_search_with_recursion(node: Node) -> None:
+
+    node.visited = True
+    print(node)
+
+    for _node in node.adjacency_list:
+        if not _node.visited:
+            depth_first_search_with_recursion(_node)
+
+
 def main():
 
     # first we have to create the node or vertices
@@ -44,7 +54,8 @@ def main():
     node4.adjacency_list.append(node5)
 
     # run DFS
-    depth_first_search(node1)
+    # depth_first_search(node1)
+    depth_first_search_with_recursion(node1)
 
 
 if __name__ == '__main__':
