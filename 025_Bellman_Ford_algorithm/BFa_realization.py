fffffddc99cbef106f11447525fef790fd326c69
Alexander Golovnya <algol.mail@gmail.com>
2021-07-02 23:31:46 +0700

    Finish realization of Bellman Ford algorithm.

--- 025_Bellman_Ford_algorithm/BFa_realization.py
+++ 025_Bellman_Ford_algorithm/BFa_realization.py
@@ -19,7 +19,7 @@ class Node:
 
 class BellmanFordAlgorithm:
 
-    def __int__(
+    def __init__(
         self,
         vertex_list: list[Node],
         edge_list: list[Edge],
@@ -78,7 +78,50 @@ class BellmanFordAlgorithm:
 
 
 def main():
-    pass
+    # we have to create the vertices
+    node1 = Node("A")
+    node2 = Node("B")
+    node3 = Node("C")
+    node4 = Node("D")
+    node5 = Node("E")
+    node6 = Node("F")
+    node7 = Node("G")
+
+    # let's create the edges
+    edge1 = Edge(5, node1, node2)
+    edge2 = Edge(9, node1, node5)
+    edge3 = Edge(4, node2, node5)
+    edge4 = Edge(12, node2, node3)
+    edge5 = Edge(7, node2, node4)
+    edge6 = Edge(3, node3, node4)
+    edge7 = Edge(1, node3, node6)
+    edge8 = Edge(9, node4, node7)
+    edge9 = Edge(6, node5, node3)
+    edge10 = Edge(4, node5, node6)
+    edge11 = Edge(2, node6, node7)
+    edge12 = Edge(6, node7, node3)
+
+    # handle the adjacency lists
+    node1.adjacency_list.append(edge1)
+    node1.adjacency_list.append(edge2)
+    node2.adjacency_list.append(edge3)
+    node2.adjacency_list.append(edge4)
+    node2.adjacency_list.append(edge5)
+    node3.adjacency_list.append(edge6)
+    node3.adjacency_list.append(edge7)
+    node4.adjacency_list.append(edge8)
+    node5.adjacency_list.append(edge9)
+    node5.adjacency_list.append(edge10)
+    node6.adjacency_list.append(edge11)
+    node7.adjacency_list.append(edge12)
+
+    # run the algorithm
+    vertices = (node1, node2, node3, node4, node5, node6, node7)
+    edges = (edge1, edge2, edge3, edge4, edge5, edge6, edge7, edge8, edge9, edge10, edge11, edge12)
+
+    algorithm = BellmanFordAlgorithm(vertices, edges, node1)
+    algorithm.find_shortest_path()
+    algorithm.get_shortest_path(node7)
 
 
 if __name__ == '__main__':
