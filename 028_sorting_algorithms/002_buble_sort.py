4bf5d50cc39f4c60f40b8ecc190498e489cb96b8
Alexander Golovnya <algol.mail@gmail.com>
2021-08-14 00:32:49 +0700

    Add sorting algorithms.

---
+++ 028_sorting_algorithms/002_buble_sort.py
@@ -0,0 +1,20 @@
+class BubbleSort:
+
+    def __init__(self, nums):
+        self.nums = nums
+
+    def sort(self):
+        for i in range(len(self.nums)-1):
+            for j in range(len(self.nums)-i-1):
+                if self.nums[j] > self.nums[j+1]:
+                    self.nums[j], self.nums[j+1] = self.nums[j+1], self.nums[j]
+
+def main():
+    # it has O(N^2) running time
+    algorithm = BubbleSort([1, -1, 0, 10, 12, -5, 1, 2, -1, 34])
+    algorithm.sort()
+    print(algorithm.nums)
+
+
+if __name__ == '__main__':
+    main()
