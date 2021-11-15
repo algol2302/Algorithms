e9fd4697cff74e2c86674b04995e972adac3dadf
Alexander Golovnya <algol.mail@gmail.com>
2021-11-16 00:38:35 +0700

    Add the next leetcode solution.

---
+++ 034_array_positive_numbers/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def algorithm(self, items: list[int]) -> int:
+        max_sum = 0
+        local_max = items[-1]
+
+        for index in range(len(items)-1, -1, -1):
+            max_sum += local_max
+            if items[index-1] > local_max:
+                local_max = items[index - 1]
+        return max_sum
+
+def main():
+    items = [1]
+    print(Solution().algorithm(items=items))
+
+
+if __name__ == '__main__':
+    main()
